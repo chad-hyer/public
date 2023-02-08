85b13031e982dfe0ea5160f8bbaf1e90e370c6c8
chad-hyer <79498208+chad-hyer@users.noreply.github.com>
2023-02-07 20:15:57 -0700

    Setup updates

--- setup1.sh
+++ setup1.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get upgrade
 sudo apt-get install git
 
 git clone https://github.com/respeaker/seeed-voicecard
 cd seeed-voicecard
 sudo ./install.sh
+
+cd
+wget https://raw.githubusercontent.com/chad-hyer/public/main/.asoundrc
 
 sudo raspi-config
