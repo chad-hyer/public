7ccc68be710100738bfc14cd801e69fc482013ac
chad-hyer <79498208+chad-hyer@users.noreply.github.com>
2023-02-08 14:49:41 -0700

    Fixes

--- toggleCall.py
+++ toggleCall.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import subprocess, os
+import subprocess, os, time
 
 GPIO.setmode(GPIO.BCM)
 
@@ -7,19 +7,30 @@ TOGGLE_PIN = 17   # Change this PIN number to your button PIN
 
 GPIO.setup(TOGGLE_PIN, GPIO.IN)
 
-value = 0
-led_last = False
+on = False
 while(True):
     input = GPIO.input(TOGGLE_PIN)
- 	
+    if input == 0:
+        if on == False:
+            print("Calling")
+            start = subprocess.run(['sudo', './RPi_Cam_Web_Interface/start.sh'])
+            input = 1
+            time.sleep(5)
+            on = True
+        elif on == True:
+            print("Ending Call")
+            subprocess.run(['sudo', './RPi_Cam_Web_Interface/stop.sh'])
+            input = 1
+            time.sleep(5)
+            on = False
+'''
     if(input == 1 and value != input):
         print("Calling")
         value = input
-        led_last = not led_last
         start = subprocess.run(['sudo', './RPi_Cam_Web_Interface/start.sh'])
-        talk = subprocess.run(['sudo', '/home/talkkonnect/bin/talkkonnect'])
+        #talk = subprocess.run(['sudo', '/home/talkkonnect/bin/talkkonnect'])
     elif(input == 0 and value != input):
         print("Ending Call")
         subprocess.run(['sudo', './RPi_Cam_Web_Interface/stop.sh'])
-        os.system('sudo pkill talkkonnect')
-        value = 0
+        #os.system('sudo pkill talkkonnect')
+        value = 0'''
